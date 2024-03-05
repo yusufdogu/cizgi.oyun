fc34f421279b1cdb079f205bbb3629fda8710dc8
Yusuf DOĞU <148912729+yusufdogu@users.noreply.github.com>
2024-03-05 17:57:54 +0300

    Add files via upload
    Fonksiyon prototipleri

---
+++ OYUN.h
@@ -0,0 +1,14 @@
+//
+// Created by ydogu on 18.02.2024.
+//
+
+#ifndef UNTITLED19_OYUN_H
+#define UNTITLED19_OYUN_H
+void oyna();
+void ciz_();
+void deger1(int *);
+void deger2(int * );
+void sifirlama();
+void oynkayit();
+void boyutla();
+#endif //UNTITLED19_OYUN_H
